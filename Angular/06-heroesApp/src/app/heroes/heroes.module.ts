fc81fe486898412d8edbee8f9c0c5d41d3421442
Eduardo Jimeno Pablo <eduardo.jimeno@bosonit.com>
2024-02-19 09:32:07 +0100

    Sección 14. Angular Material (*json-server menor versión porque no funcionaba query)

--- Angular/06-heroesApp/src/app/heroes/heroes.module.ts
+++ Angular/06-heroesApp/src/app/heroes/heroes.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { CardComponent } from './components/card/card.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
+import { MaterialModule } from '../material/material.module';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
-import { MaterialModule } from '../material/material.module';
+
 
 
 @NgModule({
@@ -16,10 +20,14 @@ import { MaterialModule } from '../material/material.module';
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    CardComponent,
+    // Pipe
+    HeroImagePipe,
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     HeroesRoutingModule,
     MaterialModule,
   ]
---
+++ Angular/06-heroesApp/src/environments/environments.prod.ts
@@ -0,0 +1,3 @@
+/* export const environments = {
+  baseUrl: 'https://app.hangar-gym.com/api'
+} */
