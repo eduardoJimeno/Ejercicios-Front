f547faa6c73a73ab33a7e1c2a9310a2dcb31dcbb
Eduardo Jimeno Pablo <eduardo.jimeno@bosonit.com>
2024-02-19 13:13:24 +0100

    Sección 16. Guards - protección de rutas

---
+++ Angular/06-heroesApp/src/app/auth/services/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environments } from '../../../environments/environments';
+import { User } from '../interfaces/user.interface';
+import { Observable, catchError, map, of, tap } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+
+  private baseUrl = environments.baseUrl;
+  private user?: User ;
+
+  constructor(private http: HttpClient) { }
+
+  get currentUser():User | undefined{
+    if ( !this.user ) return undefined;
+    return structuredClone( this.user );
+  }
+
+  login( email: string, password: string): Observable<User> {
+    // http.post('login', {email, password}):
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        tap( user => localStorage.setItem('token', 'ñálsdfkñl.poqewjf.ojqawpffn' )),
+        );
+  }
+
+  checkAuthentication(): Observable<boolean> {
+    if ( !localStorage.getItem('token') ) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user),
+        map( user => !!user ),  // con esta doble negacion me aseguro de enviar boolean de si user tiene datos
+        catchError( err => of(false) )
+      );
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
+  }
+}
