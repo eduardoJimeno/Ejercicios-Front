d086b498c6f74e85e30995c089d1d13bace54fe8
Eduardo Jimeno Pablo <eduardo.jimeno@bosonit.com>
2024-02-22 19:20:02 +0100

    Sección 22. Standalone component

--- Angular/10-mapsApp/src/app/maps/maps.module.ts
+++ Angular/10-mapsApp/src/app/maps/maps.module.ts
@@ -6,27 +6,29 @@ import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomPageComponent } from './pages/zoom-page/zoom-page.component';
+import { CounterAloneComponent } from '../alone/components/counter-alone/counter-alone.component';
+import { SideMenuComponent } from '../alone/components/side-menu/side-menu.component';
 
 
 @NgModule({
   declarations: [
-    MiniMapComponent,
-    SideMenuComponent,
-    MapsLayoutComponent,
     FullScreenPageComponent,
+    MapsLayoutComponent,
     MarkersPageComponent,
+    MiniMapComponent,
     PropertiesPageComponent,
     ZoomPageComponent,
   ],
   imports: [
     CommonModule,
+    CounterAloneComponent,
     MapsRoutingModule,
+    SideMenuComponent,
   ]
 })
 export class MapsModule { }
