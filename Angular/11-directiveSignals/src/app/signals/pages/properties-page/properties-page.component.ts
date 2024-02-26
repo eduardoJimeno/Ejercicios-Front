82acd0a4612a807402e5e9d084b0bae816e68bda
Eduardo Jimeno Pablo <eduardo.jimeno@bosonit.com>
2024-02-26 09:58:40 +0100

    Sección 24. Signals

---
+++ Angular/11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnDestroy, effect, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
+
+@Component({
+  templateUrl: './properties-page.component.html',
+  styleUrls: ['./properties-page.component.css']
+})
+export class PropertiesPageComponent implements OnDestroy{
+
+  public counter = signal(10);
+
+  public user = signal<User>({
+    id: 2,
+    email: "janet.weaver@reqres.in",
+    first_name: "Janet",
+    last_name: "Weaver",
+    avatar: "https://reqres.in/img/faces/2-image.jpg"
+  });
+
+  public userChangedEffect = effect(() => {
+    console.log(`${ this.user().first_name } - ${this.counter()}` );
+
+
+  });
+
+  ngOnDestroy(): void {
+    //this.userChangedEffect.destroy();
+  }
+
+  increaseBy (value: number){
+    this.counter.update( current => current + value );
+  }
+
+  onFieldUpdated( field: keyof User, value: string) {
+
+  /*  this.user.set({
+      ...this.user(),
+      [field]: value,
+    })
+    Peligroso porque puedo añadir campos que no existian
+    */
+    this.user.update( current => {
+
+      switch (field){
+
+        case 'email':
+          current.email = value;
+          break;
+        case 'avatar':
+          current.avatar = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'id':
+          current.id = Number( value );
+      }
+
+      return current;
+    })
+
+  }
+
+
+}
