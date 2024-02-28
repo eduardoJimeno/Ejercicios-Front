3775ab519dde7f1a07fdc97a45fd8c2f0c5120dd
Eduardo Jimeno Pablo <eduardo.jimeno@bosonit.com>
2024-02-28 08:32:52 +0100

    Sección 26. AuthApp - Backend + Angular App

---
+++ Angular/13-authApp/src/app/auth/interfaces/index.ts
@@ -0,0 +1,8 @@
+
+
+export * from './login-response.interface';
+export * from './user.interface';
+export * from './auth-status.enum';
+export * from './check-token.response';
+
+
---
+++ Angular/13-authApp/src/environments/environments.ts
@@ -0,0 +1,5 @@
+
+
+export const envrironment = {
+  baseUrl: 'http://localhost:3000'
+}
